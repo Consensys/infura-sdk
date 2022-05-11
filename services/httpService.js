a5d6bb4de7656ddb3f91dc1eefd421b12a20af9c
johann bich <johannbich@gmail.com>
2022-05-11 18:42:11 +0200

    fix: httpService test

--- services/httpService.js
+++ services/httpService.js
@@ -10,6 +10,9 @@ import axios from 'axios';
 
 export class HttpService {
   constructor(baseURL, apiKey) {
+    if (!baseURL) throw new Error('[httpService.constructor] baseURL is missing!');
+    if (!apiKey) throw new Error('[httpService.constructor] apiKey is missing!');
+
     this.instance = axios.create({
       baseURL: baseURL,
       headers: {
