b3c9477391295b33b6930b05528e01356cf2a9c4
johann bich <johannbich@gmail.com>
2022-05-12 18:18:40 +0200

    feature: e2e test / clean constant

--- e2e/createContract.test.js
+++ e2e/createContract.test.js
@@ -1,20 +1,31 @@
 import { ExternallyOwnedAccount } from '../lib/NFT/externallyOwnedAccount';
 import { addresses, private_keys } from './keys.json';
+import { config as loadEnv } from 'dotenv';
+loadEnv();
 
 describe('Onchain interaction', () => {
   let externallyOwnedAccount;
   let contract;
+  let publicAddress;
 
   beforeAll(async () => {
     // grab the first account
     const acc = Object.keys(addresses)[0];
     const PRIV_KEY = private_keys[acc];
 
+    // grab the second account for publicAddress
+    publicAddress = Object.keys(addresses)[1];
+
+    // create the apiKey
+    const apiKey = Buffer.from(`${process.env.PROJECT_ID}:${process.env.SECRET_ID}`).toString(
+      'base64',
+    );
     // call the constructor with Ganache blockchain
     externallyOwnedAccount = new ExternallyOwnedAccount({
       privateKey: PRIV_KEY,
-      apiKey: btoa(`${process.env.PROJECT_ID}:${process.env.SECRET_ID}`),
+      apiKey,
       rpcUrl: 'http://localhost:8545',
+      chainId: 4,
     });
     contract = await externallyOwnedAccount.createSmartContract('name', 'symbol');
   });
@@ -29,5 +40,19 @@ describe('Onchain interaction', () => {
     const symbol = await contract.symbol();
     expect(name).toBe('name');
     expect(symbol).toBe('symbol');
+  });
+
+  it('should return list of NFTs by address', async () => {
+    const nfts = await externallyOwnedAccount.getNFTs('0xF69c1883b098d621FC58a42E673C4bF6a6483fFf');
+    expect(nfts.assets.length).not.toBe(null);
+  });
+
+  it('should mint Nfts inside an existing contract', async () => {
+    const nfts = await externallyOwnedAccount.mintNft({
+      contractAddress: contract.address,
+      publicAddress,
+      NFTUrl: 'https://infura.io/images/404.png',
+    });
+    expect(nfts.hash).not.toBe(null);
   });
 });
