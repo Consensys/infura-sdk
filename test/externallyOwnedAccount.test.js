9caf544887e9a8be67beb2174118a437f03ab3ce
VGau <victorien.gauch@consensys.net>
2022-05-17 11:48:12 +0200

    feat: add getSymbol function

--- test/externallyOwnedAccount.test.js
+++ test/externallyOwnedAccount.test.js
@@ -1,12 +1,13 @@
-import { ExternallyOwnedAccount } from '../lib/NFT/externallyOwnedAccount';
 import { config as loadEnv } from 'dotenv';
+import { ExternallyOwnedAccount } from '../lib/NFT/externallyOwnedAccount';
+import { HttpService } from '../services/httpService';
+
 loadEnv();
+
+jest.mock('ethers');
 
 describe('ExternallyOwnedAccount', () => {
   jest.setTimeout(120 * 1000);
-  let contractAbstraction;
-
-  const NFTImage = 'https://infura.io/images/404.png';
 
   it('should throw when args are missing (privateKey)', async () => {
     expect(() => {
@@ -36,6 +37,36 @@ describe('ExternallyOwnedAccount', () => {
         rpcUrl: null,
       });
     }).toThrow('[ExternallyOwnedAccount.constructor] rpcUrl is missing!');
+  });
+
+  describe('getSymbol', () => {
+    it('should throw when contractAddress === null', async () => {
+      const account = new ExternallyOwnedAccount({
+        privateKey: 'privatekey',
+        apiKey: 'apikey',
+        rpcUrl: 'https://rinkeby.infura.io/v3/86d4a35c8d7b4509983f9f6d0623656f',
+      });
+      await expect(account.getSymbol(null)).rejects.toThrowError(
+        new Error('contractAddress cannot be null'),
+      );
+    });
+
+    it('should return the collection symbol', async () => {
+      jest.spyOn(HttpService.prototype, 'get').mockResolvedValue({
+        data: {
+          symbol: 'SYMB',
+        },
+      });
+      const account = new ExternallyOwnedAccount({
+        privateKey: 'privatekey',
+        apiKey: 'apikey',
+        rpcUrl: 'https://rinkeby.infura.io/v3/86d4a35c8d7b4509983f9f6d0623656f',
+      });
+
+      expect(await account.getSymbol('0x97ed63533c9f4f50521d78e58caeb94b175f5d35')).toStrictEqual(
+        'SYMB',
+      );
+    });
   });
 
   // Test present in E2E using Ganache instead of rinkeby
