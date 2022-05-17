611f793b7fa089fadf38776b87b228108c1701f3
johann bich <johannbich@gmail.com>
2022-05-17 12:08:27 +0200

    feature: usage proposal

--- usage.js
+++ usage.js
@@ -6,15 +6,63 @@
 
 /* eslint-disable */
 
-import { Client } from './lib/consensysClient.js';
+import { Auth, NFT, MultiNFT, Utils, Token, DAO, Gas } from '@infura/sdk';
 
-const consensysClient = new Client();
+/**
+ * Authentication object
+ * Used in most of the SDK interaction
+ * Does it need a validation (infura check? IPFS check ?)?
+ */
 
-consensysClient.authenticate({
-  accountAddress: '123',
-  key: '321',
+const account = new Auth({
+  provider: ethers.providers.Web3Provider() || rpcUrl,
+  signer: ethers.Signer() || 'privatekeystringsuperlonguesarace' || '',
+  chainId: 1 | null,
+  apiKey: 'keytoconnecttoreadAPI',
+  // infuraProjectID: "keytoconnecttoreadAPI", not sure it will be used as it will be present in rpcUrl
+  // infuraProjectSecret: "keytoconnecttoreadAPI", not sure how it will be used
+  // IPFSProjectID
+  // IPFSProjectSecret
 });
 
-consensysClient.nft.mint();
+// additional methods
+account.getNFTs(); // my NFT
+account.getNFTs(address); // get NFT for provided address
 
-export default consensysClient;
+/**
+ * NFT - Basic
+ */
+
+const myNFTBasic = new NFT({ Auth });
+const BasicContractDeployed = myNFTBasic.deploy({
+  name: 'name',
+  symbol: 'symbol',
+});
+const txReceiptBasic = BasicContractDeployed.mint(address, MetadataURI);
+// contract deployed => we can interact with all contract features, methods, ....
+
+BasicContractDeployed.name(); // 'name'
+BasicContractDeployed.symbol(); // 'symbol'
+BasicContractDeployed.grant(address, 'role');
+BasicContractDeployed.getOwner(txReceiptBasicMint.tokenID);
+
+/**
+ * NFT - Multi
+ */
+
+const myMultiNFT = new MultiNFT({ Auth });
+const MultiContractDeployed = myMultiNFT.deploy({
+  name: 'name',
+  symbol: 'symbol',
+  max_supply: 100,
+});
+
+const txReceiptMulti = MultiContractDeployed.mint(address, MetadataURI);
+// contract deployed => we can interact with all contract features, methods, ....
+
+MultiContractDeployed.name(); // 'name'
+MultiContractDeployed.symbol(); // 'symbol'
+MultiContractDeployed.maxSupply(); // 100
+MultiContractDeployed.grant(address, 'role');
+MultiContractDeployed.getOwner(txReceipt);
+MultiContractDeployed.getSupply(); // 99
