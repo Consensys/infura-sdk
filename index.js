758116dee236e226e9682f203cf6d6e079c4dffc
Fermin Carranza <fcarr004@fiu.edu>
2022-05-19 10:43:13 -0400

    go

--- index.js
+++ index.js
@@ -10,13 +10,36 @@ import { Provider } from './lib/Provider/provider.js';
 import { TEMPLATES } from './lib/NFT/contracts/templates.js';
 import Auth from './lib/Auth/index.js';
 
-const auth = new Auth();
+const auth = new Auth({
+  infuraSecret: '',
+  infuraId: '',
+  private_key: process.env.SUPER_PRIVATE,
+  rpcUrl: '',
+});
+
+// class Auth() {
+// determine whether to use infura secret and id and construct the URL
+// or use rpcUrl
+// validation of key
+// }
+
 const provider = Provider.fromJsonRPCProvider(
   'https://rinkeby.infura.io/v3/86d4a35c8d7b4509983f9f6d0623656f',
 );
 const account = new ExternallyOwnedAccount(process.env.RANDOM_PRIVATE_KEY, auth, provider);
-const contract = account.getContractFromAddress(
-  TEMPLATES.NFTContractUnlimited,
-  '0xbafDB2980eA7A82df1a7ef834B210aB4C4F06bE3',
-);
+// const contract = account.getContractFromAddress(
+//   TEMPLATES.NFTContractUnlimited,
+//   '0xbafDB2980eA7A82df1a7ef834B210aB4C4F06bE3',
+// );
+account.createSmartContract();
+contract.deploy();
 contract.name();
+
+// READ
+const api = new Api({ infuraDetails });
+const name = api.getContractMetadata(contract.address())['name'];
+contract.name();
+
+// WRITE
+contract = new BaseContract.deploy(TEMPLATES.NFTContractCollection);
+contract.mint();
