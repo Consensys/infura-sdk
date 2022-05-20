b0f3154a6360738b58f4fb2525b03151f8b801ae
johann bich <johannbich@gmail.com>
2022-05-20 15:46:35 +0200

    update based on discussion for MVP implementation

--- usage.js
+++ usage.js
@@ -12,44 +12,73 @@ import { Auth, SDK, TEMPLATE } from '@infura/sdk';
  * Authentication object
  * Used in most of the SDK interaction
  * Does it need a validation (infura check? IPFS check ?)?
+ * getProvider (injected or defaultRpcUrl)
+ * getSigner
+ * getApiKey ${projectId:secretId} | base64
  */
 
 const account = new Auth({
-  privateKey: 'privatekeystringsuperlonguesarace',
-  apiKey: 'keytoconnecttoreadAPI',
+  privateKey: 'privatekeystringverylongandveryprivate',
+  projectId: 'keytoconnecttoreadAPI',
+  projectSecret: 'keytoconnecttoreadAPI',
   rpcUrl: rpcUrl,
+  chainId: '1',
   // IPFSProjectID
   // IPFSProjectSecret
 });
+
+/**
+ * constructor
+ * deploy method with switch statement // mvp
+ * getContractMetadata(contractAddress) (name, symbol, tokenType) // mvp
+ * getNFTs(publicAddress, metadata = false) // mvp
+ * getNFTsForCollection(contractAddress) // mvp
+ * getNFTMetadata(contractAddress, tokenId) // mvp
+ * getEthBalance(publicAddress) // mvp
+ * getERC20Balance(publicAddress) // mvp
+ */
+const sdk = new SDK(account);
 
 /**
  * Read
  */
 
 const BAYC = '0x0272080AB9269C730EF802';
-const contractToRead = SDK(account).read(BAYC);
 
-contractToRead.getNFTFromCollection();
-contractToRead.getContractMetadata();
-contractToRead.getAllNFTsWihMetadata();
-contractToRead.getOwnerOf(tokenId);
+await sdk.getContractMetadata(contractAddress); // mvp (name, symbol, tokenType)
+await sdk.getNFTs(address, (metadata = false)); // mvp
+await sdk.getNFTsForCollection(contractAddress); // mvp
+await sdk.getTokenMetadata(contractAddress, tokenId); // mvp
+await sdk.getEthBalance(publicAddress); // mvp
+await sdk.getERC20Balance(publicAddress); // mvp
 
 /**
  * WRITE
+ * Deploy returns a ERC721Mintable class that you can interact with
+ * deploy and getContractFromAddress returns the same type (ERC721Mintable)
  */
 
-const { receipt, address } = SDK(account).deploy({
+const contractObject = await sdk.deploy({
   template: TEMPLATE.ERC721Mintable,
-  name: 'Cool Contract',
-  symbol: 'CC',
-  contractURI: 'ipfs://...',
-  royaltyPayee: '0x...',
-  royaltyShare: '5',
+  param: {
+    name: 'Cool Contract', // mandatory
+    symbol: 'CC', // optional
+    contractURI: 'ipfs://...', // optional
+    // royaltyPayee: '0x...',
+    // royaltyShare: '5',
+  },
+});
+
+const contractObject2 = await sdk.getContractFromAddress({
+  template: TEMPLATE.ERC721Mintable,
+  address: contractAddress,
 });
 
 const metadataUri = 'ipfs://...';
-const contractToInteract = SDK(account).interact(address);
+const to = '0x655ED967A80';
+const sharePercent = '10';
 
-const tokenId = contractToInteract.mint(to, metadataUri);
-contractToInteract.updateRoyalty('10');
-contractToInteract.transfer(to);
+// event with
+const event = await contractObject.mint(to, metadataUri);
+await contractObject.setRoyalties(to, share);
+await contractObject.transfer(to);
