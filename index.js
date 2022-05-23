fbfd49f156d403d821165d82281879c1ee436046
Daniel Pavelic <daniel.pavelic@consensys.net>
2022-05-23 13:35:56 +0100

    PTP-404: Write refatoring in progress.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
  * MIT Licensed
  */
 
-import { ExternallyOwnedAccount } from './lib/NFT/externallyOwnedAccount.js';
+import SDK from './lib/SDK/index';
+import Auth from './lib/Auth/index';
+import { TEMPLATES } from './lib/NFT/constants';
 
-export * from './lib/NFT/externallyOwnedAccount.js';
 export default {
-  ExternallyOwnedAccount,
+  SDK,
+  Auth,
+  TEMPLATES,
 };
