d5c2465f697b58fb587685365a64c720dc901cc4
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2022-05-30 15:06:35 +0200

    feat(PTP-438): add Signer class

--- test/__mocks__/utils.js
+++ test/__mocks__/utils.js
@@ -1,2 +1,8 @@
+import { randomBytes } from 'crypto';
+
 export const ACCOUNT_ADDRESS = '0xE26a682fa90322eC48eB9F3FA66E8961D799177C';
 export const CONTRACT_ADDRESS = '0x97ed63533c9f4f50521d78e58caeb94b175f5d35';
+
+export const generateTestPrivateKey = () => {
+  return `0x${randomBytes(32).toString('hex')}`;
+};
