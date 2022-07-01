001e34f2a997161781100f4e8697d469bb7dcef2
Mael Regnery <mael@mqli.fr>
2022-07-01 16:14:32 +0200

    fix: short switch linter error

--- src/lib/NFT/contractFactory.js
+++ src/lib/NFT/contractFactory.js
@@ -3,11 +3,10 @@ import ERC721Mintable from '../ContractTemplates/ERC721Mintable.js';
 
 export default class ContractFactory {
   static factory(template, signer) {
-    switch (template) {
-      case TEMPLATES.ERC721Mintable:
-        return new ERC721Mintable(signer);
-      default:
-        throw new Error('Invalid template provided');
+    if (template === TEMPLATES.ERC721Mintable) {
+      return new ERC721Mintable(signer);
     }
+
+    throw new Error('Invalid template provided');
   }
 }
