4fa9523159ccf684c19c43325e1e8bc48323c7ab
Fermin 'Piscu' Carranza <fermin.carranza@formstack.com>
2022-09-15 12:07:19 -0400

    feat(sdk): remove unsupported testnets (#90)

--- src/lib/Auth/availableChains.js
+++ src/lib/Auth/availableChains.js
@@ -1,7 +1,5 @@
 const Chains = {
   mainnet: 1,
-  ropsten: 3,
-  rinkeby: 4,
   goerli: 5,
   polygon: 137,
   mumbai: 80001,
@@ -9,8 +7,6 @@ const Chains = {
 
 const chainsName = {
   1: 'mainnet',
-  3: 'ropsten',
-  4: 'rinkeby',
   5: 'goerli',
   137: 'polygon',
   80001: 'mumbai',
@@ -18,21 +14,12 @@ const chainsName = {
 
 const chainUrls = {
   [Chains.mainnet]: 'https://mainnet.infura.io',
-  [Chains.ropsten]: 'https://ropsten.infura.io',
-  [Chains.rinkeby]: 'https://rinkeby.infura.io',
   [Chains.goerli]: 'https://goerli.infura.io',
   [Chains.polygon]: 'https://polygon-mainnet.infura.io',
   [Chains.mumbai]: 'https://polygon-mumbai.infura.io',
 };
 
-const availableChains = [
-  Chains.mainnet,
-  Chains.goerli,
-  Chains.ropsten,
-  Chains.rinkeby,
-  Chains.polygon,
-  Chains.mumbai,
-];
+const availableChains = [Chains.mainnet, Chains.goerli, Chains.polygon, Chains.mumbai];
 
 const getChainName = chainId => chainsName[chainId];
 
