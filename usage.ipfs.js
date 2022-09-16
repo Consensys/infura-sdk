4b4d900a817375e91b42df0965b77e20e716cd7d
johann bich <johannbich@gmail.com>
2022-09-16 14:20:35 +0200

    chore(usage.ipfs.js): update for PTP-1210

--- usage.ipfs.js
+++ usage.ipfs.js
@@ -7,6 +7,7 @@
 /* eslint-disable */
 
 import { config as loadEnv } from 'dotenv';
+import path from 'path';
 import { SDK, Auth, TEMPLATES, Metadata } from './index.js';
 loadEnv();
 
@@ -16,14 +17,38 @@ const ipfsUrl = process.env.INFURA_IPFS_ENDPOINT;
 
 const myMetadataStorage = new Metadata({ projectId, projectSecret, ipfsUrl });
 
+/**
+ * METADATA
+ */
+// CREATE CONTRACT URI
 // upload collection metadata from JSON object
 const collectionMetadata = {
   name: 'My awesome collection',
   description: "A long description explaining why it's awesome",
-  image: './collection_cover.png',
+  image: path.join(__dirname, 'test', 'ipfs-test/consensys.png'), // accepts absolute path and URL
   external_link: 'https://myawesomewebsite.net',
 };
-const contractURI = await myMetadataStorage.createContractURI(collectionMetadata);
+const { cid } = await myMetadataStorage.createContractURI(collectionMetadata);
+
+// upload collection metadata from JSON file
+const myjson = path.join(__dirname, 'test', 'ipfs-test/collectionMetadata.json');
+const contractURI_file = await myMetadataStorage.createContractURI(myjson);
+
+// CREATE METADATA URI
+// upload token metadata from JSON object
+const tokenMetadata = {
+  description: 'Friendly OpenSea Creature that enjoys long swims in the ocean.',
+  external_url: 'https://openseacreatures.io/3',
+  image: 'https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png',
+  animation_url: path.join(__dirname, 'test', 'ipfs-test/consensys.png'),
+  name: 'Dave Starbelly',
+  attributes: [],
+};
+const { cid: cidFile } = await myMetadataStorage.createTokenURI(tokenMetadata);
+
+// upload token metadata from JSON file
+const mynftjson = path.join(__dirname, 'test', 'ipfs-test/nftMetadata.json');
+const tokenURI_file = await myMetadataStorage.createTokenURI(mynftjson);
 
 /**
  *
@@ -45,7 +70,7 @@ const newContract = await sdk.deploy({
   params: {
     name: '1507Contract',
     symbol: 'TOC',
-    contractURI: contractURI,
+    contractURI: `https://ipfs.io/ipfs/${cid}`,
   },
 });
 console.log('contract address: \n', newContract.contractAddress);
