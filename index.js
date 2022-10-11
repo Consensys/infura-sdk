4172fe7bf4f0c6d32827c4d65cc854d73af7ef70
Johann BICH <johannbich@gmail.com>
2022-10-11 17:17:11 +0200

    fix(PTP-1428): import path & object manipulation fixed (#96)
    * fix(PTP-1428): import path & object manipulation fixed
    
    * fix: __dirname and comment duplicate metadata
    
    Co-authored-by: Mael Regnery <mael@mqli.fr>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 import Sdk from './src/lib/SDK/sdk.js';
 import Auth from './src/lib/Auth/Auth.js';
-import Metadata from 'src/lib/Metadata/Metadata.js';
+import Metadata from './src/lib/Metadata/Metadata.js';
 import { TEMPLATES } from './src/lib/NFT/constants.js';
 import Metadata from './src/lib/Metadata/Metadata.js';
 
