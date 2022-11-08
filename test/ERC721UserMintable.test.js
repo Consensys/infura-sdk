9e492c63388599a4ea8f8f6c94fbd1d9ddfe5602
Javier <javier.vera@consensys.net>
2022-11-08 12:17:01 +0100

    chore: added unit, integration and e2e tests

--- test/ERC721UserMintable.test.js
+++ test/ERC721UserMintable.test.js
@@ -3,7 +3,7 @@ import { ContractFactory, ethers } from 'ethers';
 import ERC721UserMintable from '../src/lib/ContractTemplates/ERC721UserMintable';
 import { ACCOUNT_ADDRESS, CONTRACT_ADDRESS, ACCOUNT_ADDRESS_2 } from './__mocks__/utils';
 import { errorLogger, ERROR_LOG } from '../src/lib/error/handler.js';
-
+import { jest } from '@jest/globals';
 let eRC721UserMintable;
 let signer;
 const address = '0x0';
