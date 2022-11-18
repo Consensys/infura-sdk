bd37297d25eae5c1040cda546ce6f0ee9638d1e2
Javier <javier.vera@consensys.net>
2022-11-18 09:49:39 +0100

    chore: added longer wait

--- e2e/sdk.erc721.e2e.test.js
+++ e2e/sdk.erc721.e2e.test.js
@@ -110,6 +110,7 @@ describe('SDK - contract interaction (deploy, load and mint)', () => {
     await wait(
       async () => {
         response = await sdk.getContractMetadata({ contractAddress: contract.contractAddress });
+        console.log(response);
         return response !== null;
       },
       300000,
