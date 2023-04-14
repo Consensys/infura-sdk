358cdd1528a38f743f658864ce0835d2081de2f7
GitHub Action <action@github.com>
2023-04-14 14:03:31 +0200

    fix: fix pascalCase

--- scripts/PascalCase.sh
+++ scripts/PascalCase.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-perl -pi -e 's/operations/Operations/g' ../src/services/nft-api.ts
-perl -pi -e 's/components/Components/g' ../src/services/nft-api.ts
-perl -pi -e 's/paths/Paths/g' ../src/services/nft-api.ts
-perl -pi -e 's/external/External/g' ../src/services/nft-api.ts
+perl -pi -e 's/operations/Operations/g' ./src/services/nft-api.ts
+perl -pi -e 's/components/Components/g' ./src/services/nft-api.ts
+perl -pi -e 's/paths/Paths/g' ./src/services/nft-api.ts
+perl -pi -e 's/external/External/g' ./src/services/nft-api.ts
+perl -pi -e 's/external/Webhooks/g' ./src/services/nft-api.ts
